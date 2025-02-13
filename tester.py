bfc128f0849f6aaed8228ce661cf5c3ac115d175
Maël <ma3l.bth@gmail.com>
2025-02-13 19:04:06 +0100

    feat: Add colors to the output and create a report
    The new report help to see which sections have been tested and how many tests have been passed. It's displayed at the end of the execution.

--- tester.py
+++ tester.py
@@ -13,12 +13,16 @@ SCHEMA_FILEPATH = f"{CWD}/tester/tests/schema.json"
 EXECUTER_FILEPATH = f"{CWD}/tester/script/execute.sh"
 PREPARER_FILEPATH = f"{CWD}/tester/script/prepare.sh"
 
+ERROR_LABEL = "\033[91mError\033[0m"
+WARNING_LABEL = "\033[93mWarning\033[0m"
+INFORMATION_LABEL = "\033[36mInformation\033[0m"
+
 def validate_json(json_data, schema):
     try:
         validate(instance=json_data, schema=schema)
-        print("The tests file is valid.")
+        print(f"{INFORMATION_LABEL}: The tests file is valid.")
     except ValidationError as e:
-        print(f"The tests file contains an error: {e.message}")
+        print(f"{ERROR_LABEL}: The tests file contains an error: {e.message}")
         exit(1)
 
 def load_json(filepath):
@@ -34,10 +38,9 @@ def create_function_call(name, inputs):
     return f"{name}({inputs_str})"
 
 def sanitize_workspace():
-    if path.exists(TEMPORARY_FILEPATH):
-        remove(TEMPORARY_FILEPATH)
-    if path.exists(RESULT_FILEPATH):
-        remove(RESULT_FILEPATH)
+    for file_path in [TEMPORARY_FILEPATH, RESULT_FILEPATH]:
+        if path.exists(file_path):
+            remove(file_path)
 
 def execute_test(name, args, result, test):
     try:
@@ -61,17 +64,37 @@ def execute_test(name, args, result, test):
         subprocess.run(EXECUTER_FILEPATH)
 
         with open(RESULT_FILEPATH, 'r') as file:
-            res = file.read()
+            res = file.read().strip()
             if res != test['expect']:
                 print(f"\tRunning {test['name']:<40} \033[91mKO\033[0m (Expected: '{test['expect']}', Got: '{res}')")
+                return False
             else:
                 print(f"\tRunning {test['name']:<40} \033[92mOK\033[0m")
+                return True
     except Exception as e:
         print(f"\tRunning {test['name']:<40} \033[91mKO\033[0m (Error: {e})")
+        return False
+
+def execute_tests(tests_data):
+    report = []
+
+    for section in tests_data:
+        passed = 0
+        total = 0
+
+        print(f"- Testing {section['name']}:")
+        for test in section["tests"]:
+            if execute_test(section['name'], section['args'], section['result'], test):
+                passed += 1
+            total += 1
+            sanitize_workspace()
+        report.append((section['name'], passed, total))
+    return report
 
 def main():
     parser = argparse.ArgumentParser(description="Run specific sections of the test suite.")
     parser.add_argument('-s', '--section', nargs='+', help="Specify sections to run", required=False)
+    parser.add_argument('--no-compile', action='store_true', help="Skip the preparation step", required=False)
     args = parser.parse_args()
 
     try:
@@ -81,20 +104,29 @@ def main():
         validate_json(tests_data, schema)
         sanitize_workspace()
 
-        subprocess.run(PREPARER_FILEPATH)
+        if not args.no_compile:
+            subprocess.run(PREPARER_FILEPATH)
+        else:
+            print(f"{INFORMATION_LABEL}: Skipping the library (re)compilation.")
 
-        sections_to_run = args.section if args.section else [section['name'] for section in tests_data]
+        if args.section:
+            tests_data = [section for section in tests_data if section['name'] in args.section]
 
-        for section in tests_data:
-            if section['name'] in sections_to_run:
-                print(f"Testing '{section['name']}'")
-                for test in section["tests"]:
-                    execute_test(section['name'], section['args'], section['result'], test)
-                    sanitize_workspace()
+        if not tests_data:
+            print(f"{ERROR_LABEL}: There is no section to run.")
+            exit(1)
+
+        report = execute_tests(tests_data)
+
+        print("- Report:")
+        for (section, passed, total) in report:
+            percentage = int(passed / total * 100)
+            print(f"\t{section:<40} {passed}/{total} ({percentage}%)")
+
     except FileNotFoundError as e:
-        print(f"{e.strerror}: {e.filename}")
+        print(f"{ERROR_LABEL}: {e.strerror}: {e.filename}")
     except KeyboardInterrupt:
-        print("\nUnexpected interruption.")
+        print(f"\n{WARNING_LABEL}: Unexpected interruption.")
 
 if __name__ == "__main__":
     main()
